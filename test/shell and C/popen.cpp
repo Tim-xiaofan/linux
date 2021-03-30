49456bb97ec0941f17293e245b1912de392f059d
zyj <zhongyinjiebit@sina.com>
2021-03-30 18:27:44 +0800

    pcap

--- test/shell and C/popen.cpp
+++ test/shell and C/popen.cpp
@@ -1,23 +1,27 @@
 /** 20210323 11:36, zyj ,GuangDong*/
 //创建一个连接到另一个进程（shell的命令行）的管道，然后读其输出或向其输入端发送数据
-#include<stdio.h>
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
 int main()
 {
+	using namespace std;
 	FILE *fp = NULL;
 	char data[100] = {'0'};
 	int ct = 0;
+	time_t tt;
 
-	fp = popen("top", "r");
+	fp = popen("cat ~/.bash_history ", "r");
 	if (fp == NULL)
 	{
 		printf("popen error!\n");
 		return 1;
 	}
+//	tt = time(NULL);
+//	printf("tt = %lu\n", tt);
 
-	while (fgets(data, sizeof(data), fp) != NULL)
-	{
-		printf("%03d\t%s", ++ct, data);
-	}
+	while (fgets(data, sizeof(data), fp) != NULL && (++ct) <= 10)
+		printf("%s", data);
 	pclose(fp);
 	return 0;
 }
